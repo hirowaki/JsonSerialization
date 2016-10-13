e356133e883d35986091050d1acadfddb6d0b609
hirowaki <hirowaki0314@gmail.com>
2016-10-12 17:14:28 -0700

    done

---
+++ benchmark/BenchMark.js
@@ -0,0 +1,35 @@
+'use strict';
+/*eslint-disable no-console*/
+
+const PackJson = require('./../lib/PackJson');
+const testJson = require('./SampleJson.json');
+
+function _benchMark() {
+    return Promise.resolve()
+    .then(() => {
+        console.log("no packing. Json.stringify");
+        const stringify = JSON.stringify(testJson);
+        console.log(" size: ", encodeURIComponent(stringify).length);
+
+        console.log("msgpack5.toString('base64')");
+        const packed = PackJson.packing2String(testJson);
+        console.log(" size: ", encodeURIComponent(packed).length);
+
+        console.log("zipping.toString('base64')");
+        return PackJson.zipping2base64(testJson)
+        .then((zipped) => {
+            console.log(" size: ", encodeURIComponent(zipped).length);
+            return PackJson.unzippingFromBase64(zipped)
+            .then((json) => {
+            })
+        });
+    });
+}
+
+function benchMark() {
+    return _benchMark();
+}
+
+benchMark();
+
+/*eslint-enable no-console*/
---
+++ test/PackJsonTest.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const _ = require('lodash');
+const sinon = require('sinon');
+const assert = require('assert');
+const PackJson = require('./../lib/PackJson');
+const zlib = require('zlib');
+
+describe('PackJson.', function () {
+    let sandbox;
+
+    before(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe('Pack.', function () {
+        it('packing2Buffer, unpackingFromBuffer.', function () {
+            const testJson = {sentence: "Hello World"};
+
+            const packed = PackJson.packing2Buffer(testJson);
+            assert.ok(packed instanceof Buffer);
+
+            const unpacked = PackJson.unpackingFromBuffer(packed);
+            assert.deepEqual(unpacked, testJson);
+        });
+
+        it('packing2String, unpackingFromBuffer.', function () {
+            const testJson = {sentence: "Hello World"};
+
+            const packedString = PackJson.packing2String(testJson);
+            assert.ok(_.isString(packedString));
+
+            const unpackedString = PackJson.unpackingFromString(packedString);
+            assert.deepEqual(unpackedString, testJson);
+        });
+    });
+
+    describe('Zip.', function () {
+        it('zipping2base64, unzippingFromBase64.', function () {
+            const testJson = {sentence: "Hello World"};
+
+            return PackJson.zipping2base64(testJson)
+            .then((zipped) => {
+                assert.ok(_.isString(zipped));
+
+                return PackJson.unzippingFromBase64(zipped)
+                .then((json) => {
+                    assert.deepEqual(json, testJson);
+                });
+            });
+        });
+
+        it('zipping2base64 error handler.', function () {
+            const spyZip = sandbox.stub(zlib, 'gzip', function (str, cb) {
+                void(str);
+                return cb(new Error("TEST ERROR"));
+            });
+
+            return PackJson.zipping2base64('SOURCE')
+            .then(() => {
+                assert.ok(false);   // zipping2base64 should not get resolved.
+            })
+            .catch((err) => {
+                assert.ok(spyZip.calledOnce);
+                assert.ok(spyZip.args[0][0], 'SOURCE');
+                assert.strictEqual(err.message, "TEST ERROR");
+            })
+        });
+
+        it('unzippingFromBase64 error handler.', function () {
+            const spyUnzip = sandbox.stub(zlib, 'gunzip', function (str, cb) {
+                void(str);
+                return cb(new Error("TEST ERROR"));
+            });
+
+            return PackJson.unzippingFromBase64('SOURCE')
+            .then(() => {
+                assert.ok(false);   // zipping2base64 should not get resolved.
+            })
+            .catch((err) => {
+                assert.ok(spyUnzip.calledOnce);
+                assert.ok(spyUnzip.args[0][0], 'SOURCE');
+                assert.strictEqual(err.message, "TEST ERROR");
+            })
+        });
+    });
+});
+
